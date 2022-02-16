a1f8fcaabfe2008067ef8b3cc07e2f0883e0ddbe
Pieter van der Westhuizen <pieter@csoft.co.za>
2022-02-16 08:11:56 +0200

    Initial Commit

---
+++ src/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+
+namespace QuestPdfFileSizeTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			GenerateDoc(FontNames.NotoSans);
+			GenerateDoc(FontNames.NotoSansSmall);
+			GenerateDoc(FontNames.NotoSansSmall2);
+			Console.WriteLine("========================================");
+			GenerateDoc(FontNames.Calibri);
+			GenerateDoc(FontNames.CalibriSmall);
+			Console.WriteLine("========================================");
+			GenerateDoc(FontNames.Tahoma);
+			Console.WriteLine("========================================");
+			GenerateDoc(FontNames.SegoeUI);
+			Console.WriteLine("========================================");
+			GenerateDoc(FontNames.Arial);
+
+			Console.Read();
+		}
+
+		private static void GenerateDoc(string fontName)
+		{
+			string filePath = @$"C:\temp\questpdftest_{fontName}.pdf";
+			var document = new TestDoc(fontName);
+			document.GeneratePdf(filePath);
+			Console.WriteLine($"{fontName} -> {new System.IO.FileInfo(filePath).Length / 1000} bytes");
+		}
+	}
+
+	public static class FontNames
+	{
+		public static string NotoSans = "Noto Sans";
+		public static string NotoSansSmall = "NotoSansSmall";
+		public static string NotoSansSmall2 = "NotoSansSmall2";
+
+		public static string Calibri = "Calibri";
+		public static string CalibriSmall = "CalibriSmall";
+
+		public static string Tahoma = Fonts.Tahoma;
+
+		public static string SegoeUI = Fonts.SegoeUI;
+
+		public static string Arial = Fonts.Arial;
+	}
+}
---
+++ src/TestDoc.cs
@@ -0,0 +1,88 @@
+﻿
+using QuestPDF.Drawing;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+
+namespace QuestPdfFileSizeTest
+{
+	public class TestDoc : IDocument
+	{
+		
+		private readonly string _fontName;
+
+		public TestDoc(string fontName)
+		{
+			this._fontName = fontName;
+		}
+
+		public DocumentMetadata GetMetadata() => DocumentMetadata.Default;
+
+		public void Compose(IDocumentContainer container)
+		{
+			container
+				.Page(page =>
+				{
+					page.Margin(50);
+					page.Content().Element(ComposeContent);
+				});
+		}
+
+		void ComposeContent(IContainer container)
+		{
+			container.Row(row =>
+			{
+				row.RelativeItem().Column(stack =>
+				{
+					stack.Item().Text(Faker.Lorem.Sentence(10), TextStyle.Default.FontType(_fontName).Size(18));
+					stack.Item().Text("$$ 1 2 3 4 5 6 7 8 9 0 - + < > / | ? [ ] { } %", TextStyle.Default.FontType(_fontName).Size(18));
+					stack.Item().Text(Faker.Lorem.Sentence(10), TextStyle.Default.FontType(_fontName).Size(16));
+					stack.Item().Text("$$ 1 2 3 4 5 6 7 8 9 0 - + < > / | ? [ ] { } %", TextStyle.Default.FontType(_fontName).Size(16));
+
+					for (int i = 0; i < 85; i++)
+					{
+						stack.Item().Text(Faker.Lorem.Sentence(10), TextStyle.Default.FontType(_fontName).Size(16));
+					}
+
+					stack.Item().PageBreak();
+					stack.Item().Element(ComposeTable);
+				});
+
+			});
+		}
+
+		void ComposeTable(IContainer container)
+		{
+			var headerStyle = TextStyle.Default.FontType(_fontName).Size(16);
+			var bodyTextStyle = TextStyle.Default.FontType(_fontName).Size(16);
+
+			
+			container.Table(table =>
+			{
+				table.ColumnsDefinition(columns =>
+				{
+					columns.RelativeColumn();
+					columns.RelativeColumn();
+					columns.RelativeColumn();
+				});
+
+				table.Header(header =>
+				{
+					header.Cell().Text("#", headerStyle);
+					header.Cell().Text("First Name", headerStyle);
+					header.Cell().Text("Last Name", headerStyle);
+				});
+
+				for (int i = 1; i <= 510; i++)
+				{
+					table.Cell().Element(CellStyle).Text(i, bodyTextStyle);
+					table.Cell().Element(CellStyle).Text(Faker.Name.First(), bodyTextStyle);
+					table.Cell().Element(CellStyle).Text(Faker.Name.Last(), bodyTextStyle);
+
+					static IContainer CellStyle(IContainer container) => container.BorderBottom(1).BorderColor(Colors.Grey.Lighten2).PaddingVertical(5);
+				}
+			});
+		}
+	}
+}
+
